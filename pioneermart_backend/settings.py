9a8debd0b19bf969f0525ac4808c163c11b0c035
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2025-03-15 06:47:10 -0500

    configure for phone

--- pioneermart_backend/settings.py
+++ pioneermart_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.254.30', 'localhost', '127.0.0.1']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
